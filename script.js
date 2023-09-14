3b22a3218d78d3a6b60512d6dba74b429ae50a17
RobledoSebastian <144974172+RobledoSebastian@users.noreply.github.com>
2023-09-13 23:28:13 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function mostrarInformacion(pais) {
         capital: 'Buenos Aires',
         jefeEstado: 'Agustín Pedro Justo',
         nombreOficial: 'República Argentina',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Argentina_en_1936">Argentina en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Argentina_en_1936" target="_blank">Argentina en 1936</a>',
         imagen: 'Argentina.png'
        }, 
       'Bolivia': {
@@ -27,7 +27,7 @@ function mostrarInformacion(pais) {
         capital: 'Sucre (oficial), La Paz (administrativa)',
         jefeEstado: 'José Luis Tejada Sorzano',
         nombreOficial: 'República de Bolivia',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Bolivia_en_1936">Bolivia en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Bolivia_en_1936" target="_blank">Bolivia en 1936</a>',
         imagen: 'Bolivia.jpg'
     },
       'Brasil': {
@@ -35,7 +35,7 @@ function mostrarInformacion(pais) {
         capital: 'Río de Janeiro',
         jefeEstado: 'Getúlio Vargas',
         nombreOficial: 'República de los Estados Unidos de Brasil',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Brasil_en_1936">Brasil en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Brasil_en_1936" target="_blank">Brasil en 1936</a>',
         imagen: 'Brasil.jpg'
     },
       'Chile': {
@@ -43,7 +43,7 @@ function mostrarInformacion(pais) {
         capital: 'Santiago',
         jefeEstado: 'Arturo Alessandri',
         nombreOficial: 'República de Chile',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Chile_en_1936">Chile en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Chile_en_1936" target="_blank">Chile en 1936</a>',
         imagen: 'Chile.jpg'
     },
       'Colombia': {
@@ -51,7 +51,7 @@ function mostrarInformacion(pais) {
         capital: 'Bogotá',
         jefeEstado: 'Alfonso López Pumarejo',
         nombreOficial: 'República de Colombia',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Colombia_en_1936">Colombia en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Colombia_en_1936" target="_blank">Colombia en 1936</a>',
         imagen: 'Colombia.jpg'
     },
       'Ecuador': {
@@ -59,7 +59,7 @@ function mostrarInformacion(pais) {
         capital: 'Quito',
         jefeEstado: 'Federico Páez',
         nombreOficial: 'República del Ecuador',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Ecuador_en_1936">Ecuador en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Ecuador_en_1936" target="_blank">Ecuador en 1936</a>',
         imagen: 'Ecuador.png' 
     },
       'Paraguay': {
@@ -67,7 +67,7 @@ function mostrarInformacion(pais) {
         capital: 'Asunción',
         jefeEstado: 'Eusebio Ayala',
         nombreOficial: 'República de Paraguay',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Paraguay_en_1936">Paraguay en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Paraguay_en_1936" target="_blank">Paraguay en 1936</a>',
         imagen: 'Paraguay.jpg'
     },
       'Perú': {
@@ -75,7 +75,7 @@ function mostrarInformacion(pais) {
         capital: 'Lima',
         jefeEstado: 'Óscar Benavides',
         nombreOficial: 'República Peruana',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Per%C3%BA_en_1936">Perú en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Per%C3%BA_en_1936" target="_blank">Perú en 1936</a>',
         imagen: 'Peru.jpg'
     },
       'Uruguay': {
@@ -83,7 +83,7 @@ function mostrarInformacion(pais) {
         capital: 'Montevideo',
         jefeEstado: 'Gabriel Terra',
         nombreOficial: 'República Oriental del Uruguay',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Uruguay_en_1936">Uruguay en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Uruguay_en_1936" target="_blank">Uruguay en 1936</a>',
         imagen: 'Uruguay.jpg'
     },
       'Venezuela': {
@@ -91,13 +91,10 @@ function mostrarInformacion(pais) {
         capital: 'Caracas',
         jefeEstado: 'Eleazar López Contreras',
         nombreOficial: 'Estados Unidos de Venezuela',
-        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Venezuela_en_1936">Venezuela en 1936</a>',
+        hipervinculo: '<a href="https://es.wikipedia.org/wiki/Categor%C3%ADa:Venezuela_en_1936" target="_blank">Venezuela en 1936</a>',
         imagen: 'Venezuela.jpg'
     }
     };
     
     return paises[pais];
   }
-function cerrarPagina() {
-    window.close();
-  } 
